99635ed6815a2beab6f5ae04152943846a9a90f8
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-03-26 06:03:20 +0200

    version
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- pycacher/__init__.py
+++ pycacher/__init__.py
@@ -1,1 +1,6 @@
-from .cacher import Cache as cache
+__version__ = "1.0.0"
+__author__ = "Anto I Lonappan"
+__author_email__ = "mail@antolonappan.me"
+__description__ = "A simple and powerful python cache decorator"
+
+from .cache import cache
