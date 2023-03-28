38fdd0b99c2562aac9ae9914b1c7eb00b725bb6f
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-03-28 09:06:55 +0200

    create release
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name = 'pycachera',
         packages = ['pycachera'],
         package_data = {'pycachera' : files },
-        version = '2.0.4',
+        version = '2.0.5',
         install_requires = ['numpy','pandas'],
         description = 'A powerfull python caching tool',
         author = 'Anto Idicherian Lonappan',
