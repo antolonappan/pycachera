1a9f290db59ccb82b6ff1c0e1147ef4dbb93a369
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-04-12 03:36:43 +0000

    long desc new
    Signed-off-by: GitHub <noreply@github.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 files = ["pycachera/*"]
 setup(
         name = 'pycachera',
         packages = ['pycachera'],
         package_data = {'pycachera' : files },
-        version = '3.0.1',
+        version = '3.0.2',
         install_requires = ['numpy','pandas'],
         description = 'A powerfull python caching tool',
         author = 'Anto Idicherian Lonappan',
         author_email = 'mail@antolonappan.me',
         url = 'https://github.com/antolonappan/pycachera',
+        long_description=long_description,
         long_description_content_type="text/markdown",
         )
