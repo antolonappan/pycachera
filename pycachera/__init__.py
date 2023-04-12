b1898abd30e270dca7c5d172394927c375f6254b
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-04-12 03:29:00 +0000

    long desc version
    Signed-off-by: GitHub <noreply@github.com>

--- pycachera/__init__.py
+++ pycachera/__init__.py
@@ -1,6 +1,6 @@
 from .cacher import Cache as cache
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __author__ = "Anto I Lonappan"
 __author_email__ = "mail@antolonappan.me"
 __description__ = "A simple and powerful python cache decorator"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
         name = 'pycachera',
         packages = ['pycachera'],
         package_data = {'pycachera' : files },
-        version = '3.0.0',
+        version = '3.0.1',
         install_requires = ['numpy','pandas'],
         description = 'A powerfull python caching tool',
         author = 'Anto Idicherian Lonappan',
