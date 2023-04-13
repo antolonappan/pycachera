6d4107c6064752c8994d858fdf65bd99d9e2ae29
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-04-13 03:34:05 +0000

    mpi support release
    Signed-off-by: GitHub <noreply@github.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         name = 'pycachera',
         packages = ['pycachera'],
         package_data = {'pycachera' : files },
-        version = '3.0.2',
+        version = '4.0.0',
         install_requires = ['numpy','pandas'],
         description = 'A powerfull python caching tool',
         author = 'Anto Idicherian Lonappan',
