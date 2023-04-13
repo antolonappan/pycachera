720d5a9a3f2866197ca1f3f4c53a16916725a6c4
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-04-13 05:04:46 +0000

    version fix
    Signed-off-by: GitHub <noreply@github.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         name = 'pycachera',
         packages = ['pycachera'],
         package_data = {'pycachera' : files },
-        version = '4.5.0',
+        version = '4.5.1',
         install_requires = ['numpy','pandas'],
         description = 'A powerfull python caching tool',
         author = 'Anto Idicherian Lonappan',
